f3111d8f3e9935c27ac8428e0d1814382bdfbc29
bndxn <67464282+bndxn@users.noreply.github.com>
2025-04-13 15:24:06 +0000

    Deploying to gh-pages from @ bndxn/bndxn.github.io@a38904b77555beb2812b46d673eed136b3a2a346 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,11 +52,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-run-tracker",
+          title: 'Run tracker',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/1_run_tracker/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
