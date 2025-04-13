0bf979e2c980df23d5d8c378c2d84ea7c1ec733c
bndxn <67464282+bndxn@users.noreply.github.com>
2025-04-13 15:38:31 +0000

    Deploying to gh-pages from @ bndxn/bndxn.github.io@489a25c5fe4f287007cff393ab36216b07877f84 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,11 +62,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_cycle_predictor/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-neurips-workshop-paper",
+          title: 'NeurIPS workshop paper',
+          description: "MSc dissertation",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/projects/3_neurips_paper/";
             },},{
         id: 'social-email',
         title: 'email',
