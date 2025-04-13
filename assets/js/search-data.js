40cc9e7ff1e422c47f79f46c5d74dd90df71f1e4
bndxn <67464282+bndxn@users.noreply.github.com>
2025-04-13 15:25:13 +0000

    Deploying to gh-pages from @ bndxn/bndxn.github.io@f6f5dfea342931a786da8d71826d58a02ab88889 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,11 +57,11 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_run_tracker/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-cycle-predictor",
+          title: 'Cycle predictor',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/2_cycle_predictor/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
