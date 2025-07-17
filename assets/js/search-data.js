f5e61dfa8ce4367a9be0e7d7bc5540fe7d6f76a6
ben-dixon-bbc <123097722+ben-dixon-bbc@users.noreply.github.com>
2025-07-17 15:50:46 +0000

    Deploying to gh-pages from @ bndxn/bndxn.github.io@2d416795a7c92512c2cc1b847a792cf9166ad6d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/micrograd/";
+          window.location.href = "/blog/2025/building-micrograd/";
         
       },
     },{id: "post-llm-post-training",
